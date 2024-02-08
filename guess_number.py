28ec9b56800a2f031fa3675ac27bb1907ffee892
Connor Priester <cipriester01@gmail.com>
2024-02-07 20:46:56 -0600

    Created a guess system with indicators for guesses remaining

--- guess_number.py
+++ guess_number.py
@@ -1,18 +1,25 @@
 number = 10
+max_guesses = 5
+current_guess = 0
+
 print("I'm thinking of a number...")
 
-while True:
-    guess_input = input("What number am I thinking of? (Enter 'q' to quit):  ")
+while current_guess < max_guesses:
+    guess_input = input(f"What number am I thinking of? (Enter 'q' to quit):  ")
     
     if guess_input.lower() == 'q':
         print(f"The correct number was {number}.")
         break
 
     guess = int(guess_input)
+    current_guess += 1
 
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif current_guess == max_guesses:
+        print("Sorry, you have no more guesses. The correct number was {number}.")
     else:
-        print("Sorry that is incorrect..")
+        guesses_left = max_guesses - current_guess
+        print(f"Sorry, that is incorrect. You have {guesses_left} guess(s)? left.")
 
