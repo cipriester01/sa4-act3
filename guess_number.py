d623f25ee906a87805fdc8ee82bd46a1fc91af02
Connor Priester <cipriester01@gmail.com>
2024-02-07 20:15:59 -0600

    Added feature to guesser that loops until exited or guessed correctly.

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,18 @@
 number = 10
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-if guess == number:
-	print("Congratulations! You guessed the right number.")
-else:
-	print(f"Sorry! The number was {number}.")
+
+while True:
+    guess_input = input("What number am I thinking of? (Enter 'q' to quit):  ")
+    
+    if guess_input.lower() == 'q':
+        print(f"The correct number was {number}.")
+        break
+
+    guess = int(guess_input)
+
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print("Sorry that is incorrect..")
+
